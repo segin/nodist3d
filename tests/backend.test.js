99bddea83e93a967a30aff7dc3ca204c6af2e040
Kirn Gill II <segin2005@gmail.com>
2025-06-29 14:28:16 -0500

    Feat: Initial project structure and boilerplate

---
+++ tests/backend.test.js
@@ -0,0 +1,13 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.use(express.static('src/frontend'));
+
+describe('GET /', () => {
+    it('responds with 200', (done) => {
+        request(app)
+            .get('/')
+            .expect(200, done);
+    });
+});
