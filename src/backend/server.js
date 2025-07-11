02452333c3ad3c918e382fd7dd3f46a6f5b4f295
Kirn Gill II <segin2005@gmail.com>
2025-07-11 02:26:45 -0500

    feat: add cors middleware

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,6 +3,7 @@ import log from './logger.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import helmet from 'helmet';
+import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(helmet());
+app.use(cors());
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.listen(port, '0.0.0.0', () => {
