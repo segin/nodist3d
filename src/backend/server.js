7c6c3a11cf45514d3720b57a94d8b28191dd131c
Kirn Gill II <segin2005@gmail.com>
2025-07-11 02:23:44 -0500

    feat: add helmet middleware for security

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import log from './logger.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import helmet from 'helmet';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -9,6 +10,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(helmet());
 app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.listen(port, '0.0.0.0', () => {
