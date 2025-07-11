6b228bf2324e2170f8bdb60d0d5083cc6e1a4aba
Kirn Gill II <segin2005@gmail.com>
2025-07-11 02:19:03 -0500

    fix: use path.join for static file path

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import log from './logger.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('src/frontend'));
+app.use(express.static(path.join(__dirname, '..', 'frontend')));
 
 app.listen(port, '0.0.0.0', () => {
   log.info(`Server listening at http://localhost:${port}`);
