9057a380eacac331255e572c80629a462491558b
Kirn Gill II <segin2005@gmail.com>
2025-07-11 02:36:58 -0500

    feat: implement structured logging for the backend

--- src/backend/logger.js
+++ src/backend/logger.js
@@ -1,5 +1,20 @@
 import log from 'loglevel';
 
+const originalFactory = log.methodFactory;
+log.methodFactory = (methodName, logLevel, loggerName) => {
+    const rawMethod = originalFactory(methodName, logLevel, loggerName);
+
+    return (message, ...args) => {
+        const logObject = {
+            timestamp: new Date().toISOString(),
+            level: methodName,
+            message,
+            extra: args,
+        };
+        rawMethod(JSON.stringify(logObject));
+    };
+};
+
 log.setLevel('info'); // Set the default log level
 
 export default log;
